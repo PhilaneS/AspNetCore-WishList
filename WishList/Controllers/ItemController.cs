96cb9a478d5ac499a2a4bcba4b1b815eb2f90a0c
PhilaneS <kaizer.pk@gmail.com>
2021-08-24 00:35:42 +0200

    Add Create ItemController

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
 using WishList.Models;
 
 namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View(new List<Item>() { new Item { Id=1, Description="Testing" } });
