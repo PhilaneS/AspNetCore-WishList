c1002183c0f9bcc93018098046afe0d11cb8502d
PhilaneS <kaizer.pk@gmail.com>
2021-08-24 00:21:41 +0200

    Update create index

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
@@ -10,7 +11,7 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new List<Item>() { new Item { Id=1, Description="Testing" } });
         }
     }
 }
